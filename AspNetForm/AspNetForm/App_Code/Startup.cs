62e9472711b8e7be614b4f735e35f430f810e27b
Andy Cheng <sundevilz@gmail.com>
2019-03-15 00:30:21 -0700

    Check-in initial ASP.net project.

---
+++ AspNetForm/AspNetForm/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetForm.Startup))]
+namespace AspNetForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
